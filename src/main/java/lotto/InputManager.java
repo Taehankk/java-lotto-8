0bde0b6d36337554d8fd692851c0d15788470538
KTH <1514kth@naver.com>
2025-11-01 12:52:39 +0900

    구매 금액 입력 기능 구현

--- src/main/java/lotto/InputManager.java
+++ src/main/java/lotto/InputManager.java
@@ -1,5 +1,23 @@
 package lotto;
 
-public class InputManager {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputManager {
+	
+	public int moneyInput() {
+		int money;
+		
+		System.out.println("구입 금액을 입력해 주세요.");
+		try {
+			money = Integer.parseInt(Console.readLine());		
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("숫자를 입력해주세요.");
+		}
+		
+		if(money <= 0) {
+			throw new IllegalArgumentException("양수의 숫자를 입력해주세요.");
+		}		
+		
+		return money;
+	}
 }
--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,5 +1,33 @@
 package lotto;
 
 public class LottoMachine {
-
+	LottoMachine() {}
+	
+	InputManager im = new InputManager();
+	
+	public void start() {
+		while(true) {
+			try {
+				int money = im.moneyInput();
+				int lottoCnt = purchaseLotto(money);
+				System.out.println(lottoCnt);
+				break;
+			} catch (IllegalArgumentException e) {
+				System.out.println(e);
+			}
+		
+		}
+	}
+	
+	public void end() {
+		
+	}
+	
+	public int purchaseLotto(int money) {		
+		if(money % 1000 != 0) {
+			throw new IllegalArgumentException("[Error] 1000원 단위로 입력해주세요.");
+		}
+		
+		return money / 1000;
+	}
 }
