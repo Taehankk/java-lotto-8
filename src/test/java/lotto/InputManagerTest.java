5d5b053178b5caa57f3933ac4f6eae599ace10db
KTH <1514kth@naver.com>
2025-11-03 21:17:05 +0900

    로또 번호 유효성 테스트 추가

--- src/test/java/lotto/InputManagerTest.java
+++ src/test/java/lotto/InputManagerTest.java
@@ -32,4 +32,24 @@ class InputManagerTest {
 				.isThrownBy(() -> im.validInputMoney(money))
 				.withMessage("[ERROR] 1000원 단위로 입력해주세요.");
 	}
+	
+	@DisplayName("로또 번호 유효성 테스트(6개 당첨 번호) - 구분자 잘못 입력")
+	@Test
+	void notCommaInput() {
+		String[] input = new String[] {"1.3", "4", "43", "21","11"};
+		
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> im.stringToInt(input))
+				.withMessage("[ERROR] comma(,)로 구분한 숫자 6개를 입력해 주세요.");
+	}
+	
+	@DisplayName("로또 번호 유효성 테스트(보너스 번호) - 입력값이 음수일 때")
+	@Test
+	void minusBonusNumberFailTest() {
+		int num = -21;
+		
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> im.validBonusNum(num))
+				.withMessage("[ERROR] 1에서 45 사이의 숫자 1개를 입력해 주세요.");
+	}
 }
